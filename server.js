efe6ca8328e018cf9d10a668e74d4a52ad7e084e
Andrey <brynza_andrey@mail.ru>
2020-10-31 00:37:24 +0300

    test3

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ app.use(express.static(__dirname + 'dist/tester-angular'));
 app.get('/*', function (req, res) {
   res.send("hello world");
 })
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
